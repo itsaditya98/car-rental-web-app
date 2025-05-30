a5e08801488eb5c701433f0ec60ac2a5d1852341
Aditya Vinayak <aditya.iro698s@gmail.com>
2025-05-31 00:20:27 +0530

    db update

--- app/db/db.js
+++ app/db/db.js
@@ -1,6 +1,6 @@
 
 var mongoose = require("mongoose")
-mongoose.connect('mongodb://localhost:27017/db', {
+mongoose.connect('mongodb://mongodb:27017/db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
